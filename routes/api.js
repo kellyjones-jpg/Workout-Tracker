ea408788f734abcb843cbb909a3c292e752273cd
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-02-05 19:04:06 -0700

    adding items

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.put("/api/workout/:id", ({ body }, res) => {
+    Workout.insertMany(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.get("/api/workout", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  module.exports = router;
